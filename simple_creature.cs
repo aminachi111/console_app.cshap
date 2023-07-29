b49c95c58ebe289d6a4af3132ba0e37e8b3e7ea8
Amina Mammadzade <aminamammadzada111@gmail.com>
2023-07-29 19:42:10 +0400

    avergradecommit

---
+++ simple_creature.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Backend01
+{
+    class Task
+    {
+        static void Main(string[] args)
+        {
+            /* my first program in C# */
+            Console.WriteLine(" .-.");
+            Console.WriteLine("(o o)");
+            Console.WriteLine("| 0 |");
+            Console.WriteLine("|   |");
+            Console.WriteLine("'~~~'");
+        }
+    }
+}
