18801970774593092f7f11c9f938ee13346df014
Amina Mammadzade <aminamammadzada111@gmail.com>
2023-07-29 18:16:53 +0400

    calculate_no2

---
+++ calculate_no2.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace Backend8
+{
+    class Program
+    {
+        static void Main(string[] arg)
+        {
+            int a = 6;
+            int b = 5;
+            int c = 4;
+            int d = 3;
+            int e = 2;
+
+            int result = a-- * --b + e++ / c - ++d / a * b++ - --e * c;
+
+            Console.WriteLine("Initial values:");
+            Console.WriteLine($"a: {a}");
+            Console.WriteLine($"b: {b}");
+            Console.WriteLine($"c: {c}");
+            Console.WriteLine($"d: {d}");
+            Console.WriteLine($"e: {e}");
+
+            Console.WriteLine("\nExpression Result:");
+            Console.WriteLine($"result = a-- * --b + e++ / c - ++d / a * b++ - --e * c = {result}");
+
+        }
+    }
+}
